59c330252fdb3daab349b0e52afd36ed2a39a500
Auca Maillot <auca.maillot@farmacity.com.ar>
2022-12-02 13:29:25 -0300

    :construction: start work on the first day of 2022

--- rust/2022/day_01/src/main.rs
+++ rust/2022/day_01/src/main.rs
@@ -1,3 +1,23 @@
+// 2022 - day 01
+// https://adventofcode.com/2022/day/1
+
+use std::fs;
+
 fn main() {
-    println!("Hello, world!");
+    /*
+    let filenames = fs::read_dir("./src/puzzle.input.txt").expect("oh no, we can't read the file");
+
+    for item_found in filenames {
+        println!("Name: {}", item_found.unwrap().path().display())
+    }
+    */
+
+    let file: String = fs::read_to_string("./src/puzzle-input.txt").expect("Unable to read file");
+
+    let printable_string = file.split("\r\n\r\n");
+    for item in printable_string {
+        // let number = &item.split_whitespace().as_str();
+        println!("This is the output: {:?}", item);
+        break;
+    }
 }
