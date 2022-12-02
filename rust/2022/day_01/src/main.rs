3778cb95f8255187bf37b114ce682ca8445287e0
Auca Maillot <aucacoyan@gmail.com>
2022-12-02 14:35:33 -0300

    :construction: continue the work on day-01

--- rust/2022/day_01/src/main.rs
+++ rust/2022/day_01/src/main.rs
@@ -14,10 +14,26 @@ fn main() {
 
     let file: String = fs::read_to_string("./src/puzzle-input.txt").expect("Unable to read file");
 
-    let printable_string = file.split("\r\n\r\n");
-    for item in printable_string {
-        // let number = &item.split_whitespace().as_str();
-        println!("This is the output: {:?}", item);
+    let printable_string: Vec<&str> = file.split("\r\n\r\n").collect();
+    // print the entire load of the elf
+    // println!("This is the output: {:?}", printable_string);
+    for elf in printable_string {
+        println!("This is one elf {:?}", elf);
+        // old way to do it:
+        // it returns a `Split<&str>` which prints almost jargon to the console
+        // let all_loads = elf.split("\r\n");
+        // if you split an object, it returns Split structure that have a lot of methods
+        // it doesnt return a vector with all the elements joined.
+        // You have to explicity tell rust that you want them collected
+
+        let all_loads: Vec<&str> = elf.split("\r\n").collect();
+        println!("This is the output: {:?}", all_loads);
+        let mut total_elf_load = 0;
+
+        let this_elf_load: f32 = all_loads.iter().sum();
+
+        println!("This elf has a total load of {}", total_elf_load);
+
         break;
     }
 }
