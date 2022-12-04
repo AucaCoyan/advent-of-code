85d81f9b1557607a315002623d89837dad575bf3
Auca Maillot <aucacoyan@gmail.com>
2022-12-04 09:58:13 -0300

    :sparkles: day 1 done! (first part)

--- rust/2022/day_01/src/main.rs
+++ rust/2022/day_01/src/main.rs
@@ -4,21 +4,13 @@
 use std::fs;
 
 fn main() {
-    /*
-    let filenames = fs::read_dir("./src/puzzle.input.txt").expect("oh no, we can't read the file");
-
-    for item_found in filenames {
-        println!("Name: {}", item_found.unwrap().path().display())
-    }
-    */
-
     let file: String = fs::read_to_string("./src/puzzle-input.txt").expect("Unable to read file");
 
     let printable_string: Vec<&str> = file.split("\r\n\r\n").collect();
     // print the entire load of the elf
     // println!("This is the output: {:?}", printable_string);
     for elf in printable_string {
-        println!("This is one elf {:?}", elf);
+        println!("This is one elf {:?}\n", elf);
         // old way to do it:
         // it returns a `Split<&str>` which prints almost jargon to the console
         // let all_loads = elf.split("\r\n");
@@ -27,13 +19,16 @@ fn main() {
         // You have to explicity tell rust that you want them collected
 
         let all_loads: Vec<&str> = elf.split("\r\n").collect();
-        println!("This is the output: {:?}", all_loads);
+        println!("This is the output: {:?}\n", all_loads);
         let mut total_elf_load = 0;
 
-        let this_elf_load: f32 = all_loads.iter().sum();
+        for load in all_loads.iter() {
+            let numeric_load = load.parse::<u32>().unwrap();
+            total_elf_load += numeric_load
+        }
 
-        println!("This elf has a total load of {}", total_elf_load);
-
-        break;
+        println!("This elf has a total load of {}\n", total_elf_load);
+        // break for analysing just 1 cycle
+        // break;
     }
 }
